bb82644a30ee9bd91bdbc440372d49accd5513bb
PeterpanL <116182236+PeterpanL@users.noreply.github.com>
2022-10-21 10:40:16 +0800

    Add files via upload

---
+++ menu_add.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.admin
+{
+    public partial class menu_add : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                DropDownListmenu.DataSource = Tool.menu.GetMenuList(null);
+                DropDownListmenu.DataBind();
+
+                ListItem item = new ListItem();
+                item.Text = "顶级栏目";
+                item.Value = "0";
+                DropDownListmenu.Items.Insert(0, item);
+
+            }
+        }
+
+        protected void ButtonSave_Click(object sender, EventArgs e)
+        {
+            using (var ctx = new Models.实验二Entities())
+            {
+                var menu = new Models.菜单界面表();
+                menu.id = ctx.菜单界面表.Select(s => s.id).DefaultIfEmpty().Max() + 1;
+                menu.title = TextBoxtitle.Text.Trim();
+                menu.sort_id = Tool.Tool.Obj2Int(TextBoxsort_id.Text.Trim());
+                if (DropDownListmenu.SelectedValue == "0")
+                {
+                    menu.menu_id = null;
+                }
+                else
+                {
+                    menu.menu_id = Tool.Tool.Obj2Int(DropDownListmenu.SelectedValue);
+                }
+                ctx.菜单界面表.Add(menu);
+                if (ctx.SaveChanges() > 0)
+                {
+                    Response.Redirect("menu_list.aspx");
+                }
+                else
+                {
+                    LabelAlert.Text = "添加失败";
+                }
+
+            }
+        }
+    }
+}
---
+++ user_list.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.admin
+{
+    public partial class user_list : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridViewList_RowCommand1(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Del")
+            {
+                int del_id = Tool.Tool.Obj2Int(e.CommandArgument.ToString());
+                using (var ctx = new Models.实验二Entities())
+                {
+                    var user = ctx.user.Where(s => s.id == del_id).FirstOrDefault();
+                    if (user != null)
+                    {
+                        ctx.user.Remove(user);
+
+                        ctx.SaveChanges();
+
+                        GridViewList.DataBind();
+                    }
+
+                }
+            }
+        }
+    }
+}
